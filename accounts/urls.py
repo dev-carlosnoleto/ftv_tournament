0ba98a290536f5d781e0337e4a02f90982c45e46
WandersoNoleto <noleto.wanderson.dev@gmail.com>
2025-08-12 21:18:14 -0300

    feat: add player form and register logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 urlpatterns = [
     path('choice-register', views.choice_register, name="choice_register"),
     path('register/manager', views.register_manager, name="register_manager"),
+    path('register/player', views.register_player, name="register_player"),
     path('login', views.login, name='login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from managers.models import Manager
 from managers.forms import ManagerRegistrationForm
+from players.models import Player
+from players.forms import PlayerRegistrationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -33,6 +35,36 @@ def register_manager(request):
         form = ManagerRegistrationForm()
 
     return render(request, "accounts/register_manager.html", {"form": form})
+
+def register_player(request):
+    if request.method == "POST":
+        form = PlayerRegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+                email=form.cleaned_data["email"],
+                first_name=form.cleaned_data["name"],
+            )
+
+            Player.objects.create(
+                user=user,
+                name=form.cleaned_data["name"],
+                nickname=form.cleaned_data["nickname"],
+                phone=form.cleaned_data["phone"],
+                address=form.cleaned_data["address"],
+                email=form.cleaned_data["email"],
+                height=form.cleaned_data["height"],
+                weight=form.cleaned_data["weight"],
+                age=form.cleaned_data["age"],
+            )
+
+            messages.success(request, "Conta criada com sucesso!")
+            return redirect("login")
+    else:
+        form = PlayerRegistrationForm()
+
+    return render(request, "accounts/register_player.html", {"form": form})
 
 
 def login(request):
